46dc95225b8b7263c9384051e13e4cec7ec77cde
Danijar Hafner <mail@danijar.org>
2014-05-14 00:51:58 +0200

    first

---
+++ README.md
@@ -0,0 +1,9 @@
+SAP Database Analysis
+=====================
+***REMOVED***
+Database connection
+-------------------
+***REMOVED***
+For connection to the ODBC driver we use OTL. You can find [code examples](http://otl.sourceforge.net/otl4_gen_odbc_examples.htm) in the documentation of this library.
+***REMOVED***
+Make sure you compile you application for the same architecture the ODBC database is registered for. 64bit programs can only see 64bit data sources, and the same holds for 32bit.
---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+***REMOVED***
+#define OTL_ODBC
+#include "otlv4.h"
+***REMOVED***
+using namespace std;
+***REMOVED***
+int main()
+{
+	// Initialize database driver
+	otl_connect db;
+	otl_connect::otl_initialize();
+***REMOVED***
+	try {
+		// Connect to database
+		db.rlogon("***REMOVED***;***REMOVED***;***REMOVED***");
+***REMOVED***
+		// Select from results table
+		otl_stream select(50, "SELECT parent, child, parent_ratio, child_ratio FROM ABAP.RESULT LIMIT 5", db);
+***REMOVED***
+		// Variables to hold field values of a row
+		char parent[32], child[32];
+		float pratio, cratio;
+***REMOVED***
+		// Read and print result set row by row
+		while (!select.eof()) {
+			select >> parent >> child >> pratio >> cratio;
+			cout << parent << " " << child  << " " << pratio << " " << cratio << endl;
+		}
+***REMOVED***
+	}
+	catch (otl_exception& e) {
+		// Print database errors
+		cerr << e.msg << endl;
+		cerr << e.stm_text << endl;
+		cerr << e.sqlstate << endl;
+		cerr << e.var_info << endl;
+	}
+***REMOVED***
+	// Cleanup and hold console open
+	db.logoff();
+	cin.get();
+	return 0;
+}
