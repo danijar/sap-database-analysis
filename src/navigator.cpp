f0f101952eabda7adab0162f7c48dd400a9eeff6
Danijar Hafner <mail@danijar.org>
2014-05-17 14:35:38 +0200

    hierarchie browser usability and formatting
    sort children before listing
    output as aligned table
    clear console
    help command

--- src/navigator.cpp
+++ src/navigator.cpp
@@ -1,45 +1,83 @@
 #include "navigator.h"
+#include <algorithm>
 #include <iostream>
+#include <iomanip>
+#include <cstdlib>
 #include "queries.h"
 using namespace std;
 ***REMOVED***
 // Constructor
 Navigator::Navigator(Input &Input, unordered_set<size_t> &Heads) : input(Input), heads(Heads)
 {
-	for (;;) {
-		// List current level
-		cout << endl;
-		List();
-		cout << endl;
+	Clear();
+	List();
 ***REMOVED***
-		// Navigation
-		cout << "Navigate to: ";
-		string name;
-		cin >> name;
+	for (;;) {
+		// Read user input
+		cout << endl << "> ";
+		string command;
+		cin >> command;
+		cout << endl;
 ***REMOVED***
 		// One table up
-		if (name == "..") {
-			Up();
+		if (command == "up") {
+			if (Up()) {
+				Clear();
+				List();
+			} else {
+				cout << "No previous table to go to." << endl;
+			}
 		}
 ***REMOVED***
 		// Root level
-		else if (name == "/") {
+		else if (command == "root") {
 			path.clear();
+			Clear();
+			List();
 		}
 ***REMOVED***
 		// List scheme
-		else if (name == "ls") {
+		else if (command == "scheme") {
 			if (path.size()) {
 				vector<string> fields = Queries::Schema(input.names[path.back()]);
 				for (auto i : fields)
 					cout << i << " ";
 				cout << endl;
+			} else {
+				cout << "Select a table first." << endl;
 			}
 		}
 ***REMOVED***
-		// Specified table
+		// List whole list of children
+		else if (command == "more") {
+			Clear();
+			List(0);
+		}
+***REMOVED***
+		// Exit navigator
+		else if (command == "exit" || command == "quit") {
+			Clear();
+			return;
+		}
+***REMOVED***
+		// Show available commands
+		else if (command == "help") {
+			cout << "To navigate to a table, enter its name." << endl;
+			cout << "up"     << "\t" << "Go back to the table you saw before." << endl;
+			cout << "root"   << "\t" << "Go to the root level that lists all table heads." << endl;
+			cout << "scheme" << "\t" << "List column scheme of the current table. Needs connection to database." << endl;
+			cout << "more"   << "\t" << "List all children of the current table." << endl;
+			cout << "exit"   << "\t" << "Exit the navigator. Synonyms: quit." << endl;
+		}
+***REMOVED***
+		// Navigate to table
 		else {
-			Go(name);
+			if (Go(command)) {
+				Clear();
+				List();
+			} else {
+				cout << "'" << command << "' is neither a table name nor a command. Enter 'help' to see a list of all available commands." << endl;
+			}
 		}
 	}
 }
@@ -73,45 +111,62 @@ bool Navigator::Up()
 }
 ***REMOVED***
 // List children of current table
-void Navigator::List()
+void Navigator::List(size_t Limit)
 {
-	// On highest level
+	string parent;
+	vector<Child> children;
+***REMOVED***
+	// On root level list head tables
 	if (!path.size()) {
-		// Super parents
-		cout << "Heads" << endl << "-----" << endl;
-		cout << "Found " << heads.size() << " heads." << endl;
-		int counter = 0;
-		const int limit = 20;
-		for (auto i = heads.begin(); i != heads.end() && counter < limit; ++i, ++counter) {
-			cout << input.names[*i] << " (" << Children(*i) << ")" << endl;
+		parent = "Head tables";
+		for (auto i = heads.begin(); i != heads.end(); ++i) {
+			Child child;
+			child.Name = input.names[*i];
+			child.Children = Children(*i);
+			child.Ratio = -1;
+			children.push_back(child);
 		}
-		if (heads.size() > limit)
-			cout << "..." << endl;
+***REMOVED***
+		// Sort by number of children
+		sort(children.begin(), children.end(), [](const Child &A, const Child &B) { return A.Children > B.Children; });
 	}
 ***REMOVED***
-	// Current node name
+	// For tables list their children
 	else {
 		size_t id = path.back();
-		string name = input.names[id];
-		cout << name << endl;
-		for (size_t i = 0; i < name.length(); ++i)
-			cout << "-";
-		cout << endl;
-***REMOVED***
-		// Children's names
-		if (input.ratios.find(id) == input.ratios.end()) {
-			cout << "Found no children." << endl;
-		} else {
-			cout << "Found " << input.ratios[id].size() << " children." << endl;
-			int counter = 0;
-			const int limit = 20;
-			for (auto i = input.ratios[id].begin(); i != input.ratios[id].end() && counter < limit; ++i, ++counter) {
-				cout << input.names[i->first] << " (" << Children(i->first) << ")" << endl;
+		parent = input.names[id];
+		if (input.ratios.find(id) != input.ratios.end()) {
+			for (auto i = input.ratios[id].begin(); i != input.ratios[id].end(); ++i) {
+				Child child;
+				child.Name = input.names[i->first];
+				child.Children = Children(i->first);
+				child.Ratio = Ratio(id, i->first);
+				children.push_back(child);
 			}
-			if (input.ratios[id].size() > limit)
-				cout << "..." << endl;
 		}
+***REMOVED***
+		// Sort by ratio to parent
+		sort(children.begin(), children.end(), [](const Child &A, const Child &B) { return A.Ratio > B.Ratio; });
 	}
+***REMOVED***
+	// Print children
+	Table(parent, children, Limit);
+}
+***REMOVED***
+// Get ratio between to tables
+float Navigator::Ratio(size_t From, size_t To)
+{
+	// Check for connection from parent table
+	auto i = input.ratios.find(From);
+	if (i == input.ratios.end())
+		return -1;
+***REMOVED***
+	// Check for connection to child
+	auto j = i->second.find(To);
+	if (j == i->second.end())
+		return -1;
+***REMOVED***
+	return j->second;
 }
 ***REMOVED***
 // Get number of children of a table
@@ -123,4 +178,101 @@ size_t Navigator::Children(size_t Id)
 ***REMOVED***
 	// Get number of children
 	return input.ratios[Id].size();
+}
+***REMOVED***
+// Print out the passed children as table
+void Navigator::Table(string Parent, vector<Child> &Children, size_t Limit)
+{
+	// Store default formatting
+	auto format = cout.flags();
+***REMOVED***
+	// Headline
+	cout << Parent << endl;
+	for (size_t i = 0; i < Parent.length(); ++i)
+		cout << "=";
+	cout << endl;
+***REMOVED***
+	// Print message and return if no rows
+	if (!Children.size()) {
+		cout << "Found no tables." << endl;
+		return;
+	}
+***REMOVED***
+	// Show number of rows
+	cout << "Found " << Children.size() << (Children.size() > 1 ? " tables." : " table.") << endl << endl;
+***REMOVED***
+	// Find column widths
+	size_t width_name = 0, width_children = 0, width_ratio = 4;
+	for (auto i = Children.begin(); i != Children.end(); ++i) {
+		// Name
+		if (i->Name.length() > width_name)
+			width_name = i->Name.length();
+***REMOVED***
+		// Number of Children
+		size_t number = i->Children;
+		size_t digits = 0;
+		while (number) {
+			number /= 10;
+			digits++;
+		}
+		if (digits > width_children)
+			width_children = digits;
+	}
+***REMOVED***
+	// Table head
+	if (width_name < 5)
+		width_name = 5;
+	if (width_children < 4)
+		width_children = 4;
+	if (width_ratio < 5)
+		width_ratio = 5;
+	cout << setw(width_ratio) << left << "Ratio" << " ";
+	cout << setw(width_children) << left << "Sons" << " ";
+	cout << setw(width_name) << left << "Table" << " ";
+	cout << endl;
+	for (int i = 0; i < width_ratio; ++i)
+		cout << "-";
+	cout << " ";
+	for (int i = 0; i < width_children; ++i)
+		cout << "-";
+	cout << " ";
+	for (int i = 0; i < width_name; ++i)
+		cout << "-";
+	cout << endl;
+***REMOVED***
+	// Loop up to table limit
+	for (int i = 0; i < Limit || !Limit; ++i) {
+		// Return if no more rows
+		if (i > Children.size() - 1)
+			return;
+***REMOVED***
+		// Print current row
+		Child child = Children[i];
+		if (child.Ratio > 0)
+			cout << setw(width_ratio) << right << setprecision(2) << fixed << child.Ratio << " ";
+		else {
+			for (int j = 0; j < width_ratio - 1; ++j)
+				cout << " ";
+			cout << "- ";
+		}
+		cout << setw(width_children) << right << child.Children << " ";
+		cout << setw(width_name) << left << child.Name << " ";
+		cout << endl;
+	}
+***REMOVED***
+	// Print continuation indicator
+	cout << "..." << endl;
+***REMOVED***
+	// Reset formatting options
+	cout.flags(format);
+}
+***REMOVED***
+// Clear console window on multiple platforms
+void Navigator::Clear()
+{
+#ifdef _WIN32
+	std::system("cls");
+#else
+	std::system("clear");
+#endif
 }
--- src/navigator.h
+++ src/navigator.h
@@ -10,10 +10,14 @@ public:
 	Navigator(Input &Input, std::unordered_set<size_t> &Heads);
 	bool Go(std::string Name);
 	bool Up();
-	void List();
+	void List(size_t Limit = 16);
 private:
 	Input &input;
 	std::unordered_set<size_t> &heads;
 	std::vector<size_t> path;
+	float Ratio(size_t From, size_t To);
 	size_t Children(size_t Id);
+	struct Child{ std::string Name; float Ratio; size_t Children; };
+	void Table(std::string Parent, std::vector<Child> &Children, size_t Limit = 16);
+	void Clear();
 };
