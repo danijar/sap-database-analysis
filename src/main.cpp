7d003ca18b2e7f20e35638b8bfc9d824f9625adc
koogle <jakob.frick@student.hpi.uni-potsdam.de>
2014-06-02 22:29:51 +0200

    Added a benchmarking class to measure time for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <unordered_set>
+#include <chrono>
 #include "helper/queries.h"
 #include "algorithm/ratios.h"
 #include "algorithm/hierarchy.h"
 #include "algorithm/structures.h"
+#include "tool/benchmark.h"
 #include "tool/navigator.h"
-#include "helper\jsonize.h"
+#include "helper/jsonize.h"
 using namespace std;
 ***REMOVED***
 ***REMOVED***
@@ -17,19 +20,43 @@ int main()
 	Queries::User = "***REMOVED***";
 	Queries::Password = "***REMOVED***";
 ***REMOVED***
+	
+	if (BENCHMARK) {
+		Benchmark::Start_Clock();
+	}
 	// Fetch input
 	Ratios ratios;
+	
+	// Take the time and print it
+	if (BENCHMARK) {
+		double duration = Benchmark::Stop_Clock().count();
+		cout << "Loading the Ratios took " << std::setprecision(5) << duration << " seconds" << endl;
+		Benchmark::Start_Clock();
+	}
 ***REMOVED***
 	// Build hierarchy
 	Hierarchy hierarchy(ratios);
+	
+	// Take the time and print it
+	if (BENCHMARK) {
+		double duration = Benchmark::Stop_Clock().count();
+		cout << "Loading the Hirachy took " << std::setprecision(5) << duration << " seconds" << endl;
+		Benchmark::Start_Clock();
+	}
 ***REMOVED***
 	// Fetch structures
 	Structures structures(ratios, hierarchy);
 ***REMOVED***
+	// Take the time and print it
+	if (BENCHMARK) {
+		double duration = Benchmark::Stop_Clock().count();
+		cout << "Loading the Structues took " << std::setprecision(5) << duration << " seconds" << endl;
+	}
+***REMOVED***
 	// Wait for user input
-	// cout << "Press enter to start hierarchy navigator..." << endl;
-	// cout.flush();
-	// cin.get();
+	cout << "Press enter to start hierarchy navigator..." << endl;
+	cout.flush();
+	cin.get();
 	
 	// Navigate through hierarchie
 	Navigator navigator(hierarchy, structures);
