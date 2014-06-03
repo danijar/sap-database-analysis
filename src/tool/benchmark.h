ed9f5b805427d31e183011ea71a6ad672ec8a8be
koogle <jakob.frick@student.hpi.uni-potsdam.de>
2014-06-03 17:53:21 +0200

    before testing commit

--- src/tool/benchmark.h
+++ src/tool/benchmark.h
@@ -1,20 +1,40 @@
 #pragma once
+#define BENCHMARK true
 #include <chrono>
 #include <ctime>
 #include <ratio>
 ***REMOVED***
-namespace Benchmark {
-	bool BENCHMARK = true;
-	// Benchmark is a namespace for time measurement
-	using namespace std::chrono;
+// I don't exactly know why i have to do this, some problem with multiple definitions @Danijar
+namespace {
 ***REMOVED***
-	bool runs = false;
-	high_resolution_clock::time_point start;
+	// Benchmark is a static class for time measurement
+	struct Benchmark {
+		static bool runs;
+		static std::chrono::high_resolution_clock::time_point start;
 ***REMOVED***
-	// Starts the clock if it s not already running
-	bool Start_Clock();
+		// Starts the clock if it s not already running
+		static bool Start_Clock(){
+			if (!runs && BENCHMARK) {
+				start = std::chrono::high_resolution_clock::now();
+				runs = true;
+				return true;
+			}
+			else
+				return false;
+		}
 ***REMOVED***
-	// Stops the clock and returns time duration between start and stop if clock was running
-	duration<double> Stop_Clock();
+		// Stops the clock and returns time duration between start and stop if clock was running
+		static std::chrono::duration<double> Stop_Clock(){
+			if (!runs || !BENCHMARK)
+				return std::chrono::duration<double>();
+***REMOVED***
+			std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
+			runs = false;
+			return std::chrono::duration_cast<std::chrono::duration<double>>(end - start);
+		}
+	};
+***REMOVED***
+	bool Benchmark::runs = false;
+	std::chrono::high_resolution_clock::time_point Benchmark::start;
 ***REMOVED***
 }
