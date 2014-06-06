8a7a6858ec42b5b63ea788d2e030b0386cee7574
Danijar Hafner <mail@danijar.org>
2014-06-06 18:14:48 +0200

    launchpad for viewer

--- viewer/js/main.js
+++ viewer/js/main.js
@@ -16,6 +16,38 @@ requirejs.config({
     }
 });
 ***REMOVED***
-require(['view'], function(View) {
-    View();
+require(['tree', 'launchpad'], function(View, Launchpad) {
+    var container = $('.container');
+***REMOVED***
+    // Initialize routes
+    var routes = {
+        '/?':              function() { Launchpad(container); },
+        '/table/(.+)/?': function(table) { View(container, table); },
+    };
+***REMOVED***
+    // Listen to route changes
+    $(window).on('hashchange', function() {
+        // Find matching route
+        var hash = window.location.hash.substr(1);
+        for (var i in routes) {
+            // Test regex against current route
+            var regex = new RegExp('^' + i + '$', 'gm');
+            var matches = regex.exec(hash);
+            if (matches) {
+                // Cleanup old view
+                container.children().remove();
+***REMOVED***
+                // Create view with captured groups
+                var args = matches.slice(1, matches.length);
+                routes[i].apply(undefined, args);
+                return;
+            }
+        }
+        
+        // No route found
+        console.error("No route matches the hash '" + hash + "'.");
+    });
+***REMOVED***
+    // Load initial view
+    $(window).trigger('hashchange');
 });
