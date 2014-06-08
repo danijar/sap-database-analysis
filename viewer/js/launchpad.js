9574c702cfc4f6d019ab06e4ee4a298711cfc6b9
Danijar Hafner <mail@danijar.org>
2014-06-08 18:33:44 +0200

    viewer fix and styling
    special chars related fix
    show amount of both direct and overall children
    improved styling
    fixed jakob's messed formatting an dcomments
    removed unused hierarchy()

--- viewer/js/launchpad.js
+++ viewer/js/launchpad.js
@@ -28,7 +28,7 @@ define(['jquery', 'underscore', 'text!../../data/root/children.json', 'text!../.
 		var tile = $('<a class="tile" href="#/table/' + current + '">');
 		tile.append('<h2>' + current + '</h2>');
 		tile.append(difference);
-		tile.append('<p>' + amounts[current] + ' children</p>');
+		tile.append('<p>' + amounts[current] + ' tables</p>');
 ***REMOVED***
 		element.append(tile);
 	}
@@ -37,7 +37,15 @@ define(['jquery', 'underscore', 'text!../../data/root/children.json', 'text!../.
 	function main(container) {
 		initialize(container);
 ***REMOVED***
-		_.each(children['root'], function(head) {
+		// Sort head tables by cluster size
+		var heads = children['<root>'];
+		heads = _.sortBy(heads, function(head) {
+			return amounts[head];
+		});
+		heads.reverse();
+***REMOVED***
+		// Add head tables as tiles
+		_.each(heads, function(head) {
 			tile(head);
 		});
 	}
