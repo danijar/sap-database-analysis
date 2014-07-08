24dfe840edbeb24bb93bffd1b1011191795bad23
Danijar Hafner <mail@danijar.org>
2014-07-08 15:38:20 +0200

    write change values into result schema (not working)

--- src/type/field.h
+++ src/type/field.h
@@ -7,14 +7,6 @@
 // Struct for the field values
 struct Field {
 	bool operator==(const Field &other) const;
-***REMOVED***
-	// Implement a find function for the difference check
-	struct find : std::unary_function<Field, bool> {
-		std::string name;
-		find(Field f) : name(f.name) { }
-		bool operator()(Field const& f) const { return f.name == name; }
-	};
-***REMOVED***
 	std::string name, roll, domain;
 	size_t position;
 };
